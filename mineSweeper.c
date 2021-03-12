9b49a8fd38771ddb0f2b0697b5feca9ca7735f7a
pranigopu <69959590+pranigopu@users.noreply.github.com>
2021-03-12 22:48:03 +0530

    Add files via upload

---
+++ mineSweeper.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mineSweeper.h"
+int main() 
+{
+    int i;
+    char c, cmd[10];
+    do 
+    {
+        for(i = 0; i < MAX; i++)
+            display[i] = ' ';
+        system("clear");
+        printf("\nDEADLY MINEFIELD\n(a. k. a. minesweeper)\n\n");
+        printf("You are trapped in a dungeon filled with hidden mines ready to detonate. Find them all!\n");
+        printf("\nEnter ? to know the game\nEnter x to exit\nEnter anything else to play\n");
+        fseek(stdin, 0, 0);
+        while((c = getchar()) == ' ' || c == '\t');
+        if(c == 'x')
+        {
+            while((c = getchar()) == ' ' || c == '\t');
+            if(c == '\n')
+                return 0;
+        }
+        else if(c == '?')
+        {
+            while((c = getchar()) == ' ' || c == '\t');
+            if(c == '\n')
+            {
+                printf("\nHOW TO PLAY\n");
+                printf("\nThe numbers in the boxed postions show the number of mines around that position.\n");
+                printf("\nTo select a postion, enter the row and column separated by a comma, like\n");
+                printf("<row_number> , <column_number>\ne.g. 2,3\n");
+               
+                printf("\nYou can select multiple positions by separating each position with a '+' sign, like\n");
+                printf("2,3 + 4,5\n");
+
+                printf("\nIf the selected position doesn't have a mine, then you will see\n");
+                printf("1. The number of mines around the selected position\n");
+                printf("2. The number of mines around the adjacent positions, if they don't contain mines\n");
+                printf("\nIf you want to mark a place that you think has a mine, prefix the position with an 'f'.\n");
+                printf("e.g. f2,3\n");
+                printf("\nYou can flag multiple positions like\n");
+                printf("f2,3 + 4,5\nThis will flag both (2,3) and (4,5)\n");
+                printf("\nDon't worry about spacing, just be mindful of the order.\n");
+                
+                printf("\nIf you uncover all positions without selecting a mine and mark all the mines, you win.\n\n");
+                goto END;
+            }
+        }
+        else
+        {
+            //GAME BEGINS
+            placemines();
+            int flag = 0;
+            do
+            {
+                printf("\nfreeplaces = %d\n", freeplaces);
+                board();
+                do
+                {
+                    flag = 0;
+                    printf("\n--> ");
+                    fseek(stdin, 0, 0);
+                    for(i = 0; i < 10; i++)
+                    {
+                        while((c = getchar()) == ' ' || c == '\t');
+                        if(c == '\n')
+                        { 
+                            cmd[i] = '\0';
+                            if((i = error(cmd, flag)) == 1)
+                            {
+                                printf("\nInvalid command.\n");
+                                break;
+                            }
+                            else if(i == -1)
+                                goto END;
+                            if(update(i, flag) == -2)
+                                goto END;
+                            break;
+                        }
+                        cmd[i] = c; 
+                        if(c == '+')
+                        {
+                            if((i = error(cmd, flag)) == 1)
+                            {
+                                printf("\nInvalid command.\n");
+                                break;
+                            }
+                            else if(i == -1)
+                                goto END;
+                            else if(i == FLAG)
+                                flag = 1;
+                            if(update(i, flag) == -2)
+                                goto END;
+                            i = -1;
+                        } 
+                    }
+                } while(i == 1);
+                if(freeplaces == 0)
+                {
+                    printf("\nYou lived to see another day!\n");
+                    goto END;
+                }
+            } while(1);
+        }
+        END:
+        fseek(stdin, 0, 0);
+        getchar();
+    } while(1);
+    return 0;
+}
---
+++ mineSweeper.h
@@ -0,0 +1,276 @@
+﻿#include <stdio.h>
+#define length 9
+#define MAX length * length + 1
+#define FLAG 7
+
+unsigned int seed;
+int mines;
+int freeplaces = MAX;
+char playspace[MAX];
+char display[MAX];
+int pos;
+
+//RANDOMLY PLACES MINES
+void placemines()
+{
+    void correct(unsigned int *);
+    unsigned int temp;
+    int i, j;
+    fseek(stdin, 0, 0);
+    printf("\nEnter seed: ");
+    scanf("%u", &seed);
+    temp = seed;
+    for(i = 0, j = 0; i < MAX - 1; i++)
+    {
+        temp *= temp + temp / 7;
+        correct(&temp);
+        if(temp % 17 < 2)
+        {
+            playspace[j] = '1';
+            mines++;
+        }
+        else
+            playspace[j] = '0';
+        j++;
+         
+    }
+    playspace[j] = '\0';
+}
+void correct(unsigned int *n)
+{
+    int i, j;
+    unsigned int temp;
+    temp = *n;
+    for(i = 0; temp > 0; i++)
+    {
+        temp /= 10;
+    }
+
+    if(i > 4)
+        for(j = 0; j < i - 4; j++)
+            *n /= 10;
+    else
+        for(j = 0; j < 4 - i; j++)
+        {
+            *n = *n * 13;
+        }
+}
+
+//COUNTS ADJACENT MINES
+int minecounter(int p, int iterations)
+{
+    int i;
+    int minecount = 0;
+    int temp = 0, u = 0; 
+    for(i = 0; i < iterations; i++)
+    {
+        switch(i)
+        {
+            case 0:
+                if(playspace[p - 1] == '1' && p % length != 0)
+                    minecount++;
+                break;
+            case 1:
+                if(playspace[p + 1] == '1' && (p + 1) % length != 0)
+                    minecount++;
+                break;
+            case 2:
+                if(p + length < MAX && playspace[p + length] == '1')
+                    minecount++;
+                temp += minecounter(p + length, 2);
+                break;
+            case 3:
+                if(p - length > -1 && playspace[p - length] == '1')
+                    minecount++;
+                temp += minecounter(p - length, 2);
+                break;
+        }
+    }
+    return minecount + temp;
+}
+
+//SHOWS THE INTERFACE
+void board()
+{
+    int i, j;
+    printf("\n    ");
+    for(i = 0; i < length ; i++)
+        printf(" %d  ", i + 1);
+    for(i = 0; i < MAX - 1; i++)
+    {
+        if(i % length == 0)
+        {
+            printf("\n    ");
+            if(i != 0)
+                for(j = 0; j < length; j++)
+                {
+                    if(j < length - 1)
+                        printf(" - +");
+                    else 
+                        printf(" - ");
+                }
+            printf("\n%d   ", i / length + 1);
+        }
+        if((i + 1) % length == 0)
+            printf(" %c ", display[i]);
+        else
+            printf(" %c |", display[i]);
+    }
+    printf("\n");
+}
+
+//CHECKS FOR AN ERROR IN THE COMMANDS
+int error(char *cmd, int flag)
+{
+    int row, col, i = 0;
+    int rval = 0;
+    if(cmd[0] == 'x' && cmd[1] == '\n')
+        return -1;
+    if(cmd[0] == 'f' || flag == 1)
+    {
+        rval = FLAG;
+        if(cmd[0] == 'f')
+            i = 1;
+    }
+//-------------------------
+    //Reading the position
+    if(cmd[i] < '0' || cmd[i] > '9')
+        return 1;
+    else
+        row = cmd[i] - '0';
+    if(cmd[i + 1] != ',')
+        return 1;
+    if(cmd[i + 2] < '0' || cmd[2] > '9')
+        return 1;
+    else
+        col = cmd[i + 2] - '0';
+//-------------------------
+    if(cmd[i + 3] != '\0' && cmd[i + 3] != '+')
+        return 1;
+//-------------------------
+    if(row < 1 || row > length)
+        return 1;
+    if(col < 1|| col > length)
+        return 1;
+    pos = length * row - (length - col) - 1;
+    return rval;
+}
+
+//ADJACENT FILLER
+void adjacentfiller(int p, int x, int hcall, int vcall)
+{
+    if(p < 0 || p > MAX - 1)
+        return;
+    int i, j;
+    if((j = minecounter(p, 5)) != 0 && x > 1)
+    {
+        if(display[p] == ' ')
+            --freeplaces;
+        display[p] = j + '0';
+        return;
+    }
+    if(display[p] == ' ')
+        --freeplaces;
+    display[p] = j + '0'; 
+    for(i = 0; i < x; i++)
+    {
+        switch(i)
+        {
+            case 0:
+                if(p % length != 0 && playspace[p - 1] == '0' && hcall != 2)
+                {
+                    if((j = minecounter(p - 1, 5)) != 0)
+                    {
+                        if(display[p - 1] == ' ')
+                            --freeplaces;
+                        display[p - 1] = j + '0';
+                    }
+                    else if(x > 1)
+                        adjacentfiller(p - 1, 2, 1, 0);
+                }
+                if((p + 1) % length != 0 && playspace[p + 1] == '0' && hcall != 1)
+                {
+                    if((j = minecounter(p + 1, 5)) != 0)
+                    {
+                        if(display[p + 1] == ' ')
+                            --freeplaces;
+                        display[p + 1] = j + '0';
+                    }
+                    else if(x > 1)
+                        adjacentfiller(p + 1, 2, 2, 0);
+                }  
+                break; 
+            case 1:
+                if((p - length) > -1 && playspace[p - length] == '0' && vcall != 2)
+                {
+                    if((j = minecounter(p - length, 5)) != 0)
+                    {
+                        if(display[p - length] == ' ')
+                            --freeplaces;
+                        display[p - length] = j + '0';
+                    }
+                    else
+                        adjacentfiller(p - length, 2, hcall, 1);
+                    adjacentfiller(p - length, 1, 0, 0);
+                } 
+                if((p + length) < MAX && playspace[p + length] == '0' && vcall != 1)
+                {
+                    if((j = minecounter(p + length, 5)) != 0)
+                    {
+                        if(display[p + length] == ' ')
+                            --freeplaces;
+                        display[p + length] = j + '0';
+                    }
+                    else
+                        adjacentfiller(p + length, 2, hcall, 2);
+                    adjacentfiller(p + length, 1, 0, 0);
+                } 
+        }
+    }
+}
+
+//WHEN YOU GO TO A MINE AND DON'T FLAG IT, THIS IS WHAT YOU SEE
+void explode()
+{
+    int i = 0;
+    for(i = 0; i < MAX; i++)
+        if(playspace[i] == '1')
+            display[i] = 'X';
+    board();
+}
+
+//BOARD UPDATER
+int update(int i, int flag)
+{
+    int k, j, p;
+    if(i == FLAG || flag == 1)
+    {
+        if(display[pos] == ' ')
+        {
+            display[pos] = 'F';
+            freeplaces--;
+            if(playspace[pos] == '1')
+                mines--;
+        }
+        else if(display[pos] == 'F')
+        {
+            freeplaces++;
+            mines++;
+            display[pos] = ' ';
+        }
+    }
+    else
+    {
+        if(playspace[pos] == '0' && display[pos] == ' ')
+            adjacentfiller(pos, 3, 0, 0);
+        else if(display[pos] != ' ')
+            printf("\nYou've already covered position (%d, %d).\n", (pos + length) / length, (pos + 1) % length);
+        else
+        {
+            explode();
+            printf("\nYou were blown to smithereens. R. I. P.\n\n");
+            return -2;
+        }
+    }
+    return 0;
+}
